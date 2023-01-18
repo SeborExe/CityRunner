5be1ef55e26009ee0e4485011b2be1109100997a
Sebastian Hara <48924353+SeborExe@users.noreply.github.com>
2023-01-18 20:41:37 +0100

    background
    Background with parallax effect has been created.

---
+++ City Runner/Assets/__Scripts/Parallax.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Parallax : MonoBehaviour
+{
+    private float length;
+    private float startPos;
+    private GameObject cam;
+
+    [SerializeField] float parallaxEffect;
+
+    private void Start()
+    {
+        cam = Camera.main.gameObject;
+
+        startPos = transform.position.x;
+        length = GetComponent<SpriteRenderer>().bounds.size.x;
+    }
+
+    private void FixedUpdate()
+    {
+        float temp = (cam.transform.position.x) * (1 - parallaxEffect); // how for moved relativity to camera
+        float distance = (cam.transform.position.x * parallaxEffect);
+
+        transform.position = new Vector3(startPos + distance, transform.position.y, transform.position.z);
+
+        if (temp > startPos + length)
+        {
+            startPos += length;
+        }
+        else if (temp < startPos - length)
+        {
+            startPos -= length;
+        }
+    }
+}
